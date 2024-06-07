15ab4a8e7225865ef1ee5dc1a45a07306fdf27e3
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-06-07 14:08:03 +0200

    start menu remover + enable debug console

---
+++ ToastManager.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using InControl;
+using TMPro;
+
+namespace DebugMod;
+
+public class ToastManager {
+    public const float MaxToastAge = 5;
+
+    private record struct ToastMessage(float StartTime, string Text);
+
+    private bool toastsDirty = false;
+    private List<ToastMessage> toasts = [];
+
+    private TMP_Text toastText;
+
+    public void Toast(string message) {
+        toasts.Add(new ToastMessage(InputManager.CurrentTime, message));
+        toastsDirty = true;
+    }
+
+
+    public void Initialize(TMP_Text text) {
+        toastText = text;
+    }
+
+
+    public void Update() {
+        var now = InputManager.CurrentTime;
+        toastsDirty |= toasts.RemoveAll(toast => now - toast.StartTime > MaxToastAge) > 0;
+
+        if (toastsDirty) toastText.text = string.Join('\n', toasts.Select(toast => toast.Text));
+    }
+}
