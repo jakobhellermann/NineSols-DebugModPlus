894a62e0a35fce453dc97cc15b89a595ca8b9956
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-08-04 22:00:40 +0200

    add UnlockMaps method

--- Source/Modules/CheatModule.cs
+++ Source/Modules/CheatModule.cs
@@ -12,4 +12,13 @@ public class CheatModule {
             player.chiContainer.GainFull();
         }
     }
+
+    [BindableMethod(Name = "Unlock all maps")]
+    private static void UnlockMaps() {
+        foreach (var areaCollection in GameCore.Instance.allAreas.allCollections)
+        foreach (var data in areaCollection.levelMapDatas) {
+            data.Unlocked.CurrentValue = true;
+            foreach (var x in data.MistMapDataEntries) x.BindingFlag.CurrentValue = true;
+        }
+    }
 }
