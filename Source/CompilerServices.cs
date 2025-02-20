6414c3de1b848cc9070661ecbd79c134836511d7
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-02-20 18:55:26 +0100

    refactor: consolidate CompilerServices

--- Source/CompilerServices.cs
+++ Source/CompilerServices.cs
@@ -1,9 +1,14 @@
 // ReSharper disable once CheckNamespace
+// ReSharper disable UnusedType.Global
 
 namespace System.Runtime.CompilerServices;
 
-public class RequiredMemberAttribute : Attribute {
+// Enable `string property { get; init; }`
+internal static class IsExternalInit {
 }
+
+// Enable `required string x`
+public class RequiredMemberAttribute : Attribute;
 
 public class CompilerFeatureRequiredAttribute : Attribute {
     public CompilerFeatureRequiredAttribute(string name) {
--- Source/IsExternalInit.cs
+++ Source/IsExternalInit.cs
@@ -1,6 +1,3 @@
 // ReSharper disable CheckNamespace
 
 namespace System.Runtime.CompilerServices;
-
-internal static class IsExternalInit {
-}
