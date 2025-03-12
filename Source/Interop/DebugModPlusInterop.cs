4b23c9cf74dbf3f81934479e052fb4b3a24848d5
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-03-12 19:51:11 +0100

    add interop class

---
+++ Source/Interop/DebugModPlusInterop.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Linq;
+using DebugModPlus.Savestates;
+using NineSolsAPI;
+
+namespace DebugModPlus.Interop;
+
+public static class DebugModPlusInterop {
+    public static void CreateSavestate(string name, string? layer, int filter) {
+        var savestate = SavestateLogic.Create((SavestateFilter)filter);
+        DebugModPlus.Instance.SavestateModule.Savestates.Save(name, savestate, null, layer);
+    }
+
+    public static bool LoadSavestate(string name, string? layer) {
+        if (DebugModPlus.Instance.SavestateModule.Savestates.TryGetValue(name, out var savestate, layer)) {
+            var task = SavestateLogic.Load(savestate, SavestateLoadMode.None);
+            if (!task.IsCompleted) {
+                ToastManager.Toast("Did not load savestate instantly");
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public static string[] ListSavestates(string? layer) {
+        return DebugModPlus.Instance.SavestateModule.Savestates.List(null, layer)
+            .Select(x => Path.GetFileNameWithoutExtension(x.path))
+            .ToArray();
+    }
+}
