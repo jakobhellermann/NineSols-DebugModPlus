9cd88c48ec0125584f01b31855c991f86c376b4c
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-03-31 21:33:20 +0200

    savestate: prevent OnRebindAnimatorMove during loading
    good for TAS determinism

---
+++ Source/Savestates/Patches.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using DebugModPlus.Interop;
+using HarmonyLib;
+
+namespace DebugModPlus.Savestates;
+
+// ReSharper disable once InconsistentNaming
+[HarmonyPatch]
+public class Patches {
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Actor), nameof(Actor.OnRebindAnimatorMove))]
+    [HarmonyPatch(typeof(Player), nameof(Player.OnRebindAnimatorMove))]
+    public static bool PreventDuringLoad(MethodBase __originalMethod) => !DebugModPlusInterop.IsLoadingSavestate;
+}
